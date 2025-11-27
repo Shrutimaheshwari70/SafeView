d2fb85333218b2629370c541a18c002f4fdc2817
Shruti Maheshwari <your_email@example.com>
2025-11-27 19:37:51 +0530

    Fixed import path for db.js in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import connectDB from './config/db.js';
+import connectDB from './src/config/db.js';
 
 import authRoutes from './routes/authRoutes.js';
 import parentRoutes from './routes/parentRoutes.js';
